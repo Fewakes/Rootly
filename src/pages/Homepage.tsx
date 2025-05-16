74023287069d9758df9d4a1023bad3539889fd98
Fewakes <Slojkowski33@gmail.com>
2025-05-16 18:25:24 +0300

    Finalising rough sketch of auth

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,10 @@
+import LogoutButton from '@/components/SignoutButton';
 import React from 'react';
 
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <LogoutButton />
+    </div>
+  );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { supabase } from '../lib/supabaseClient';
 import { GalleryVerticalEnd } from 'lucide-react';
 import { LoginForm } from '@/components/login-form';
+import loginImage from '../assets/LoginLogo.png';
 
 // Initial Login page
 export default function Login() {
@@ -14,27 +15,28 @@ export default function Login() {
   };
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src={loginImage}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            Acme Inc.
+            Rootly
           </a>
         </div>
+
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             <LoginForm handleLogin={handleLogin} />
           </div>
         </div>
-      </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
       </div>
     </div>
   );
